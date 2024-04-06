344711f8b4d9990e155f7063b1fc682f8a0a4bca
iankristoper <iankristoper26@gmail.com>
2024-04-06 20:49:32 +0800

    UPDATE: sub class features

---
+++ src/main/java/com/ian/messaging_framework/EmailSender.java
@@ -0,0 +1,18 @@
+package com.ian.messaging_framework;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+public class EmailSender implements MessageSender
+{
+    
+    //Override the methods in the interface
+    @Override
+    public void sendMessage(String recipient, String message)
+    {
+        // Logic to send email message
+        System.out.println("Email sent to " + recipient + ": " + message);
+    }
+}
