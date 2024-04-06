12b9356b1dc755a52b8db55f32a312bbaaca2496
iankristoper <iankristoper26@gmail.com>
2024-04-06 20:49:10 +0800

    UPDATE: main class features

---
+++ src/main/java/com/ian/messaging_framework/Messaging_Framework.java
@@ -0,0 +1,23 @@
+package com.ian.messaging_framework;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+public class Messaging_Framework 
+{
+
+    public static void main(String[] args) 
+    {
+        //creating instances of message senders 
+        MessageSender emailSender = new EmailSender();
+        MessageSender smsSender = new SMSSender();
+        MessageSender pushNotificationSender = new PushNotifications();
+        
+        // Send messages using different protocols
+        emailSender.sendMessage("example@example.com", "Hello, this is an email message!");
+        smsSender.sendMessage("+123456789", "Hello, this is an SMS message!");
+        pushNotificationSender.sendMessage("user123", "Hello, this is a push notification message!");
+    }
+}
